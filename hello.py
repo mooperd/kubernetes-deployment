3e900267774c0980c63f795a5d48a0f29b79d1e2
Andrew Holway <andrew.holway@gmail.com>
2021-01-11 10:15:36 +0100

    changed message

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     if 'THIS_SERVICE' in os.environ:
-        return_string = "Hello World. This is service <b>{0}</b> built from branch <b>{1}</b>"\
+        return_string = "Hello Cedric and Thomas. This is service <b>{0}</b> built from branch <b>{1}</b>"\
         .format(os.environ['THIS_SERVICE'], os.environ['BRANCH'])
     else:
         return_string = "Hello World" 
