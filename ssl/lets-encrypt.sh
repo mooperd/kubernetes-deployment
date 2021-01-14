018012569ef3d16002c98828bb5693aaa89c397b
Andrew Holway <andrew.holway@gmail.com>
2021-01-14 11:17:38 +0000

    more lets-encrypt-logic

--- ssl/lets-encrypt.sh
+++ ssl/lets-encrypt.sh
@@ -2,7 +2,12 @@
 # This script needs bash4 or above
 
 if [[ ! -v 'TLD' ]]; then
-    echo "TLD is empty or undefined. Please set \$TLD as the top level domain"
+    echo "var TLD is empty or undefined. Please set \$TLD as the top level domain"
+    exit 1
+fi
+
+if [[ ! -v 'NAMESPACE' ]]; then
+    echo "var NAMESPACE is empty or undefined. Please set \$NAMESPACE so I know which k8s namespace to place the cert in."
     exit 1
 fi
 
@@ -15,6 +20,13 @@ fi
 
 mkdir -p $TLD
 
+if [[ ! -v 'NAMESPACE' ]]; then
+    echo "var NAMESPACE is empty or undefined. Please set \$NAMESPACE so I know which k8s namespace to place the cert in."
+    exit 1
+fi
+
+# create fake cert if --fake flag is passed.
+if [[ $* == *--fake ]]; then	
 certbot certonly \
       --test-cert \
       --dns-route53 -d *.$TLD \
@@ -24,6 +36,21 @@ certbot certonly \
       -m andrew.holway@guest.hpi.de \
       --agree-tos -n 
 
+# create real cert if --real flag is passed.
+elif [[ $* == *--real ]]; then
+certbot certonly \
+      --dns-route53 -d *.$TLD \
+      --config-dir ~/$TLD \
+      --work-dir ~/$TLD \
+      --logs-dir ~/$TLD \
+      -m andrew.holway@guest.hpi.de \
+      --agree-tos -n 
+else
+echo "You need to choose between a fake or real ssl cert. Please pass --fake or --real flags"
+exit 1
+fi
+
 kubectl create secret tls wildcard-${TLD/./-} \
       --key ~/$TLD/live/$TLD/privkey.pem \
-      --cert ~/$TLD/live/$TLD/fullchain.pem  
+      --cert ~/$TLD/live/$TLD/fullchain.pem \
+      -n $NAMESPACE
